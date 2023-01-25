30028919c187546d235f784364f8208bd7d616d7
evanbrownv4 <evanbrown066@gmail.com>
2023-01-25 15:17:35 +0000

    Made the playspace and window independent of each other for the most part, cleaned up a few things and broke a few others

--- snake/snakeTutorial.py
+++ snake/snakeTutorial.py
@@ -3,39 +3,47 @@ import time
 import random
 from collections import deque
 
-size = 40
+unit_size = 40
+
+window_size = (1000, 600)
+
+playspace_dimensions = ((0, 1), (25, 15))
+
+colours = {"White": (255, 255, 255), "Black": (0, 0, 0), "Light Green": (150, 250, 150), "Dark Green": (50, 150, 50)}
+
 
 class Apple:
 
     def __init__(self, parent_screen, x, y):
-        self.apple_img = pygame.image.load("/Users/evan/PycharmProjects/AHProject/snake/Resources/apple.jpeg").convert()
+        self.apple_img = pygame.image.load("Resources/apple.jpeg").convert()
         self.parent_screen = parent_screen
-        self.coord = (x, y)
+        self.x = x
+        self.y = y
 
     def draw(self):
-        self.parent_screen.blit(self.apple_img, self.coord)
+        self.parent_screen.blit(self.apple_img, (self.x*40, self.y*40))
 
 
 class Snake:
     snake_size = 5
-    starting_x = 3 * size
-    starting_y = 3 * size
+    starting_x = 3 + playspace_dimensions[0][0]
+    starting_y = 3 + playspace_dimensions[0][1]
 
     def __init__(self, parent_screen):
         self.parent_screen = parent_screen
 
         # Assigns this object representing an image to the variable Block
-        self.block = pygame.image.load("/Users/evan/PycharmProjects/AHProject/snake/Resources/block.jpeg").convert()
+        self.block = pygame.image.load("Resources/block.jpeg").convert()
 
         self.x = deque([self.starting_x]*(self.snake_size-1))
-        self.x.appendleft(self.starting_x + size)
-        self.y = deque([self.starting_y + size]*self.snake_size)
+        self.x.appendleft(self.starting_x + 1)
+        self.y = deque([self.starting_y + 1]*self.snake_size)
 
         self.direction = None
 
     def draw(self):
         for x, y in zip(self.x, self.y):
-            self.parent_screen.blit(self.block, (x, y))
+            self.parent_screen.blit(self.block, (x * unit_size, y * unit_size))
 
     def grow(self, coordinates):
         self.x.append(coordinates[0])
@@ -71,27 +79,27 @@ class Snake:
         if direction == "Up":
             # For example if moving up, calculate the new coordinates for the head, append it to the front/left of
             # the list
-            self.y.appendleft(self.y[0] - size)
+            self.y.appendleft(self.y[0] - 1)
             self.x.appendleft(self.x[0])
 
             # Then pop the last coordinates in the list and return it in case we need to add it back after growing
             return self.x.pop(), self.y.pop()
 
         elif direction == "Down":
-            self.y.appendleft(self.y[0] + size)
+            self.y.appendleft(self.y[0] + 1)
             self.x.appendleft(self.x[0])
 
             return self.x.pop(), self.y.pop()
 
         elif direction == "Left":
             self.y.appendleft(self.y[0])
-            self.x.appendleft(self.x[0] - size)
+            self.x.appendleft(self.x[0] - 1)
 
             return self.x.pop(), self.y.pop()
 
         elif direction == "Right":
             self.y.appendleft(self.y[0])
-            self.x.appendleft(self.x[0] + size)
+            self.x.appendleft(self.x[0] + 1)
 
             return self.x.pop(), self.y.pop()
 
@@ -101,17 +109,13 @@ class Snake:
 
 class Game:
 
-    running = True
-
     def __init__(self):
         # Initialise pygame
         pygame.init()
-        # Initialise window/screen of some size as surface
-        self.surface = pygame.display.set_mode((1000, 600))
-        # Does something to the surface being displayed (Filling it with white)
-        self.surface.fill((255, 255, 255))
+        # Initialise window/screen of some unit_size as surface
+        self.surface = pygame.display.set_mode(window_size)
 
-        self.num_of_apples = 2
+        self.num_of_apples = 3
         self.score = 0
 
         self.dead = False
@@ -124,8 +128,8 @@ class Game:
             self.create_apple()
 
     def draw_new_frame(self):
-        # Empty the whole screen
-        self.surface.fill((255, 255, 255))
+        # Redraw background
+        self.draw_background()
         # Draw snake on top
         self.snake.draw()
 
@@ -139,13 +143,30 @@ class Game:
         # Refresh screen
         pygame.display.flip()
 
+    def draw_background(self):
+        # Fill screen
+        self.surface.fill(colours["White"])
+        # Fill playspace with another colour
+        self.surface.fill(colours["Light Green"],
+                          rect=(playspace_dimensions[0][0] * unit_size,
+                                playspace_dimensions[0][1] * unit_size,
+                                playspace_dimensions[1][0] * unit_size,
+                                playspace_dimensions[1][1] * unit_size)
+                          )
+        # Draw border
+        pygame.draw.lines(self.surface, colours["Black"], True,
+                          [(playspace_dimensions[0][0]*unit_size-1, playspace_dimensions[0][1]*unit_size-1),
+                           (playspace_dimensions[0][0]*unit_size-1, playspace_dimensions[1][1]*unit_size),
+                           (playspace_dimensions[1][0]*unit_size, playspace_dimensions[1][1]*unit_size),
+                           (playspace_dimensions[1][0]*unit_size, playspace_dimensions[0][1]*unit_size-1)])
+
     def move(self):
         # Move the snake, keep the last position of its tail/segment just popped in case we need to add it back
         popped_coordinates = self.snake.walk(self.snake.direction)
 
         # If the head of the snake collides with the coordinates of one of the apples:
         for i in range(self.num_of_apples):
-            if (self.snake.x[0], self.snake.y[0]) == self.apples[i].coord:
+            if (self.snake.x[0], self.snake.y[0]) == (self.apples[i].x, self.apples[i].y):
 
                 # Remove collided apple from list of apples
                 self.apples.pop(i)
@@ -165,7 +186,8 @@ class Game:
 
     def check_collision(self):
         # Check if the snake has moved out of bounds of the surface
-        if (not (0 < self.snake.x[0] < 1000)) or (not (0 < self.snake.y[0] < 600)):
+        if (not (playspace_dimensions[0][0] < self.snake.x[0] < playspace_dimensions[1][0])) or (
+                not (playspace_dimensions[0][1] < self.snake.y[0] < playspace_dimensions[1][1])):
             self.dead = True
         # Check if the snake has collided with itself
         elif (self.snake.x[0], self.snake.y[0]) in [(self.snake.x[i], self.snake.y[i]) for i in range(1, self.snake.snake_size)]:
@@ -173,10 +195,11 @@ class Game:
 
     def create_apple(self):
         # Start with a list of all positions within bounds of the screen
-        all_positions = [(x * size, y * size) for x in range(25) for y in range(15)]
+        all_positions = [(x, y) for x in range(playspace_dimensions[0][0], playspace_dimensions[1][0])
+                         for y in range(playspace_dimensions[0][1], playspace_dimensions[1][1])]
         # Get a list of all positions occupied by apples and snakes
         # Apples
-        apple_positions = [apple.coord for apple in self.apples]
+        apple_positions = [(apple.x, apple.y) for apple in self.apples]
         # Snake
         snake_positions = [(self.snake.x[i], self.snake.y[i]) for i in range(self.snake.snake_size)]
 
@@ -196,7 +219,8 @@ class Game:
     def display_score(self):
         font = pygame.font.SysFont("arial", 30)
         score_counter = font.render(f"Score: {self.score}", True, (0, 0, 0))
-        self.surface.blit(score_counter, (0, 0))
+        score_counter_position = (0, 0)
+        self.surface.blit(score_counter, score_counter_position)
 
     def game_over(self):
         # Draw the game over screen
@@ -217,7 +241,7 @@ class Game:
         button_quit = font.render("Quit :(", True, (0, 0, 0))
         self.surface.blit(button_quit, (566, 200))
 
-        # Refresh the screen once (I don't need to animate anything, so I'll only loop to get mouse positions for buttons
+        # Refresh the screen once
         pygame.display.flip()
 
         while self.dead:
@@ -242,11 +266,17 @@ class Game:
                         pygame.quit()
 
     def game_restart(self):
-        print("Restart")
-        self.__init__()
+        self.dead = False
+
+        # Create snake
+        self.snake = Snake(self.surface)
+        # Create apples
+        self.apples = []
+        for x in range(self.num_of_apples):
+            self.create_apple()
 
     def run(self):
-        while self.running:
+        while True:
             # Run this while playing
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
@@ -262,14 +292,14 @@ class Game:
                         self.snake.move_left()
 
                 if event.type == pygame.QUIT:
-                    running = False
+                    quit()
+
+            if self.dead:
+                self.game_over()
 
             self.move()
 
             self.draw_new_frame()
-
-            if self.dead:
-                self.game_over()
             
             time.sleep(0.2)
 
